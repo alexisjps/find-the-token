497fa7ad2bc6cb03b8f877510ae6a8e6efdbe844
Alexis Stephan <alexis-stephan@hotmail.fr>
2023-04-12 23:43:54 +0200

    done
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,4 @@
 class GamesController < ApplicationController
-
-  def show
-    @game = Game.find(params[:id])
-    elapsed_time = @game.updated_at - @game.created_at
-    @minutes = elapsed_time.to_i / 60
-    @seconds = elapsed_time.to_i % 60
-  end
-
   def new
     @game = Game.new
     session[:secret_code] = generate_secret_code if session[:secret_code].nil?
@@ -20,11 +12,11 @@ class GamesController < ApplicationController
     session[:attempts] = session[:attempts].to_i + 1
     @game.attempts = session[:attempts]
     puts "Secret code: #{session[:secret_code]}"
-    
+  
     if session[:start_time].nil?
       session[:start_time] = Time.now.to_i
     end
-    
+  
     if @game.save
       result = compare_codes(@game.secret_code, @game.user_guesses)
       flash[:notice] = "Indice: #{result}. Nombre de tentatives: #{@game.attempts}."
@@ -35,28 +27,14 @@ class GamesController < ApplicationController
         minutes = elapsed_time / 60
         seconds = elapsed_time % 60
         flash[:notice] = "Félicitations, vous avez trouvé le code secret ! Vous avez réussi en #{minutes} minute(s) et #{seconds} seconde(s) et en #{@game.attempts} tentatives."
-        session[:start_time] = nil # réinitialiser le temps de départ pour le prochain essai
-        respond_to do |format|
-          format.turbo_stream {
-            render turbo_stream: turbo_stream.append(:game_result, partial: "games/result")
-          }
-          format.html {
-            redirect_to new_game_path, notice: flash[:notice]
-          }
-          format.js {
-            flash[:notice] = "#{flash[:notice]}\\nCeci est une alerte de pop-up."
-            render :popup
-          }
-        end
-      else
-        redirect_to new_game_path
       end
+      session[:start_time] = Time.now.to_i # Mettre à jour le temps de départ pour le prochain essai
+      redirect_to new_game_path
     else
       flash[:alert] = "Erreur lors de la création du jeu. Veuillez réessayer."
       render :new
     end
   end
-  
 
   private
 
